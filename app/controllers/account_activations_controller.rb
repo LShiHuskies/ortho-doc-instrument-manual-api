018353fa20dfa51aa71fb02c38bdaae34a952037
yoonsung <jeffkim1118@outlook.com>
2023-07-10 19:23:05 -0400

    heroku config

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,7 +1,6 @@
 class AccountActivationsController < ApplicationController
     def edit
         @user = User.find_by(email: params[:email])
-        
         token = get_token(payload(@user.username, @user.id))
         # if @user && !@user.activated? && @user.authenticated?(:activation, token)
         if @user && !@user.activated? && true
